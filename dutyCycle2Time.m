212d91398617e8d939908afe56bf2ff12688c2c1
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-01-31 16:58:57 -0500

    Added the printout function Tom wrote

--- dutyCycle2Time.m
+++ dutyCycle2Time.m
@@ -9,3 +9,29 @@ function [t_1,t_0] = dutyCycle2Time(carrier_frequency,duty_cycle)
 %
 %readme:
 %This function will convert the duty cycle, a realtive number, into two quantities of time that depend on the carrier frequency, or the frequency of the pulses.
+%Function to take in t as 2xNMAX matrix (units milliseconds) and open up v2 
+%(drug) for ton (t(1,:)), then
+%open up v3(no drug) for toff (t(2,:))
+
+function printout(time_series)
+
+%Print out header and start psi
+
+fid = fopen('protocol.txt','wt')
+fprintf(fid, '%% M04s Microfluidic Plate \n%% Basic Protocol \n \n \nsetflow X 4.000000 \nsetflow Y 4.000000 \n\n','%','%')
+
+%Find out total length of array
+tmp=size(time_series);
+NMAX=tmp(2)
+
+for i=1:NMAX
+    %Units must be in minutes
+    ton=time_series(1,i)/(60000); 
+    toff=time_series(2,i)/(60000);
+    fprintf(fid,'%% Step %d \n',i);
+    fprintf(fid,'open v2 \nwait %6.6f \nclose v2 \n',ton);
+    fprintf(fid,'open v3 \nwait %6.6f \nclose v3 \n \n',toff); 
+end
+
+fprintf(fid,'end');      
+fclose(fid);
