7201f9889ff3833fc06f40f349b87e27242a5771
Kyle Karhohs <gandalfisarockstar@gmail.com>
2012-02-01 13:11:44 -0500

    This function now handles different carrier frequencies and step functions.

--- pwm_intersective_method.m
+++ pwm_intersective_method.m
@@ -1,4 +1,4 @@
-function [pwm_lead,pwm_trail] = pwm_intersective_method(y_0, x_0, y_1, x_1)
+function [pwm_lead,num_pulses] = pwm_intersective_method(y_0, x_0, y_1, x_1,carrier_period)
 %input:
 %y_0, x_0, y_1, x_1 = two points that make a line. These two points should
 %be from a "drug waveform". The waveform will have previously been
@@ -9,6 +9,7 @@ function [pwm_lead,pwm_trail] = pwm_intersective_method(y_0, x_0, y_1, x_1)
 %extra day in a leap year, the pwm_time_error keeps track of this building
 %difference and when it exceeds the length of a single pulse it an extra
 %pulse is added (or left out).
+%carrier_period: in milliseconds;
 %
 %output:
 %pwm = a 2 x N matrix that stores duty-cycles for t_on and t_off data. N =
@@ -33,33 +34,34 @@ sawtooth = @(x) x;
 for i=1:number_of_pulses
     g = @(x) line_input(x+i-1)-sawtooth(x); %The waveform being translated into pulses slides to the left the length of one pulse every iteration of the loop. The intersection is found by finding the root of the difference between the two lines.
     pwm(1,i) = fzero(g,0.5); %fzero finds the position where a curve crosses the x-axis closest to 0.5. 0.5 starts the search in the middle of a pulse.
-    if pwm(1,i) > 0.95 %The ONIX valve system requirs 9ms to switch a valve on and off. The minimum pulse width was chosen such that a 1% duty-cycle is this length. A 5% (95%) duty-cycle minimum (maximum) ensures that the valves have sufficient time to turn on or off.
+    pulse_time = ceil(pwm(1,i)*carrier_period);
+    if pulse_time > (carrier_period-19) %The minimum pulse width the ONIX system will handle with integrity is 18ms.
         pwm(1,i) = 1;
-    elseif pwm(1,i) < 0.05
+    elseif pulse_time < 19
         pwm(1,i) = 0;
     end
     pwm(2,i) = 1 - pwm(1,i);
 end
 
 %<debug note="plots the modulate waveform and the PWM signal">
-figure
-plot ((0:number_of_pulses),line_input(0:number_of_pulses),'r')
-pulsetrain=ones(1,2*number_of_pulses);
-my_temp = 2:2:2*number_of_pulses; %evens
-pulsetrain(my_temp) = 0;
-hold
-my_temp = 0:number_of_pulses-1;
-my_temp2 = my_temp + pwm(1,:);
-my_temp2 = sort([my_temp my_temp2]);
-stairs(my_temp2,pulsetrain);
-hold off 
-figure
-my_fun = @(x1,x2) m_input*0.5*(x1.^2-x2.^2)+b_input*(x1-x2);
-plot((0:number_of_pulses-1),my_fun(my_temp,my_temp(1)),'r')
-hold
-plot((1:number_of_pulses),cumsum(pwm(1,:)))
-hold off
+% figure
+% plot ((0:number_of_pulses),line_input(0:number_of_pulses),'r')
+% pulsetrain=ones(1,2*number_of_pulses);
+% my_temp = 2:2:2*number_of_pulses; %evens
+% pulsetrain(my_temp) = 0;
+% hold
+% my_temp = 0:number_of_pulses-1;
+% my_temp2 = my_temp + pwm(1,:);
+% my_temp2 = sort([my_temp my_temp2]);
+% stairs(my_temp2,pulsetrain);
+% hold off 
+% figure
+% my_fun = @(x1,x2) m_input*0.5*(x1.^2-x2.^2)+b_input*(x1-x2);
+% plot((0:number_of_pulses-1),my_fun(my_temp,my_temp(1)),'r')
+% hold
+% plot((1:number_of_pulses),cumsum(pwm(1,:)))
+% hold off
 %</debug>
 
 pwm_lead = pwm(1,:);
-pwm_trail = pwm(2,:);
+num_pulses = length(pwm_lead);
